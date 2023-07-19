e4bdcf588f0da2fc7406c7ee1760f4fc3bbb5925
NamaAnda <NamaEmail>
2023-07-18 22:47:05 -0700

    menambah kan resourc

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ database = "sqlite:///" + os.path.join(basedir, "db.sqlite")
 app.config["SQLALCHEMY_DATABASE_URI"] = database
 
 class ModelDatabase(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    nama = db.column(db.String(100))
-    umur = db.colunb(db.Integer)
-    alamat = db.column(db.TEXT)
+    id = db.Column(db.Integer, primary_key=True)
+    nama = db.Column(db.String(100))
+    umur = db.Column(db.Integer)
+    alamat = db.olumn(db.TEXT)
 
     def seve (self):
         try:
@@ -38,7 +38,21 @@ identitas = {}
 
 class ContohReource(Resource):
     def get(self):
-        return identitas
+        
+        query = ModelDatabase.query.all()
+        output = [{
+            "nama":data.nama, 
+            "umur":data.umur, 
+            "alamat":data.alama}
+                for data in query]
+
+        response = {
+                "code" : 200,
+                "msg"  : "query data berhasil" ,
+                "data" : output
+            }
+
+        return output, 200
     
     def post(self):
         dataNama = request.form["nama"]
@@ -46,15 +60,44 @@ class ContohReource(Resource):
         dataAlamat = request.form["alamat"]
 
         model = ModelDatabase(nama=dataNama, umur=dataUmur, alamat=dataAlamat)
-        model.save()
+        model.seve()
 
         response = {"msg" : "Data berhasil di masukan",
                     "code" : 200
         
         }
         return response, 200 
+    
+class Naruto(Resource):
+    def put(self, id):
+        query = ModelDatabase,query.get(id)
+        editNama = request.form["nama"]
+        editUmur = request.form["umur"]
+        editAlamat = request.form["alamat"]
+
+        query.nama = editNama
+        query.umur = editUmur
+        query.alamat = editAlamat
+
+        response = {
+            "msg" : "edit data berhasil",
+            "cede" : 200
+        }
+        return response
+    
+    def delete(self, id):
+        queryData = ModelDatabase.query.get(id)
+        db.session.delete(queryData)
+        db.session.commit()
+
+        response = {
+            "msg" : "delete data berhasil",
+            "code" : 200
+        }
+        return response
 
 api.add_resource(ContohReource, "/api", methods=["GET", "POST"])
+api.add_resource(Naruto, "/api/<id>", methods=["PUT"])
 
 
 if __name__ == "__main__":
